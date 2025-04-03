7c457c75306956caba42a39f0a3c23c78e46c684
username <email>
2025-04-03 05:44:14 +0530

    commit message

---
+++ hello.py/odd or even.py
@@ -0,0 +1,11 @@
+def check_even_odd(num):
+    if num % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+num = int(input("Enter a number: "))
+print(check_even_odd(num))
+def check_even_odd(num):
+    return "Even" if num % 2 == 0 else "Odd"
+num = int(input("Enter a number: "))
+print(check_even_odd(num))
